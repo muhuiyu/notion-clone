61a406c76d6183bfdab17e5afca79556dfbef0c6
muhuiyu <muhuiyu.grace@gmail.com>
2024-02-28 22:03:06 +0800

    Add menu to edit document page

--- app/(main)/_components/NavBar.tsx
+++ app/(main)/_components/NavBar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react"
 import { MenuIcon } from "lucide-react"
 import { useParams } from "next/navigation"
 import Banner from "./Banner"
+import Menu from "./Menu"
 import Title from "./Title"
 
 interface Props {
@@ -19,8 +20,11 @@ const NavBar = ({ isCollapsed, onResetWidth }: Props) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     )
   }
@@ -35,6 +39,9 @@ const NavBar = ({ isCollapsed, onResetWidth }: Props) => {
         {isCollapsed && <MenuIcon role="button" onClick={onResetWidth} className="h-6 w-6 text-muted-foreground" />}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
       {document.isArchived && <Banner documentId={document._id} />}
